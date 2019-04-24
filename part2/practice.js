885f7af40e72c96a5e45b4676429c81f5130d1a5
peterallanmdavid <peterallanmdavid@gmail.com>
2019-04-24 21:09:13 +0800

    completed part2 test and corrected the script to run the test.

--- part2/practice.js
+++ part2/practice.js
@@ -1,98 +1,129 @@
-function dummy () {
-  return 1;
+function dummy() {
+  return 1
 }
 /**
  * Reverse an array
- * 
+ *
  * @example
- * 
+ *
  * reverse([1,2,3]) returns [3,2,1]
- * 
+ *
  * @param {array} array
  * @returns {array}
  */
-function reverse (array) {
-  return [];
+function reverse(array) {
+  return array.reverse()
 }
 
 /**
  * Reverse a string
- * 
+ *
  * @example
- * 
+ *
  * stringReverse('hello') returns 'olleh'
- * 
+ *
  * @param {string} text
  * @returns {string}
- * 
+ *
  */
-function stringReverse (text) {
-  return '';
+function stringReverse(text) {
+  const textArray = text.split('')
+  const reversedArray = textArray.reverse()
+  return reversedArray.join('')
 }
-
 
 /**
  * return a fixed length fibonacci list
- * 
+ *
  * @example
- * 
+ *
  * fibonacci(5) returns [1,1,2,3,5]
- * 
+ *
  * @param {number} length
  * @returns {array}
  */
-function fibonacci (length) {
-  return [];
+function fibonacci(length) {
+  if (length < 3) {
+    return [1, 1]
+  } else {
+    const fibList = fibonacci(length - 1)
+    const fibListLength = fibList.length
+    fibList.push(fibList[fibListLength - 1] + fibList[fibListLength - 2])
+    return fibList
+  }
 }
 
 /**
  * return the biggest number in the array
- * 
+ *
  * @example
- * 
+ *
  * biggest([1,3,5,7,9]) returns 9
- * 
+ *
  * @param {array} array
  * @returns {number}
  */
-function biggest (array) {
-  return 0;
+function biggest(array) {
+  const sortedArray = array.sort((a, b) => a - b)
+  return sortedArray.pop()
 }
 
 /**
- * Creates an array of numbers progressing from 
+ * Creates an array of numbers progressing from
  * start up to, but not including, end
- * 
+ *
  * YOU HAVE TO IMPLEMENT YOUR OWN ALGORITHM HERE!!!
  * DON'T USE ANY 3RD PARTY LIBRARIES.
- * 
+ *
  * @example
- * 
+ *
  * range(0,4) returns [0,1,2,3]
- * 
- * @param {number} start 
- * @param {number} end 
+ *
+ * @param {number} start
+ * @param {number} end
  * @returns {array}
  */
-function range (start, end) {
-  return [];
+function range(start, end) {
+  let rangedArray = []
+
+  const myFunction = newStart => {
+    if (newStart < end) {
+      rangedArray.push(newStart)
+      myFunction(newStart + 1)
+    }
+  }
+
+  myFunction(start)
+  return rangedArray
 }
 
 /**
  * Recursively flattens array.
- * 
+ *
  * YOU HAVE TO IMPLEMENT YOUR OWN ALGORITHM HERE!!!
  * DON'T USE ANY 3RD PARTY LIBRARIES.
- * 
+ *
  * @example
- * 
+ *
  * flatten([1,[2,[3,4],[5,[6]]]]) returns [1,2,3,4,5,6]
- * 
+ *
  * @param {array} array
  * @returns {array}
  */
-function flatten (array) {
-  return [];
+function flatten(array) {
+  let flattenedArray = []
+  const iterateIfArray = arr => {
+    if (Array.isArray(arr)) {
+      for (let i = 0; i < arr.length; ++i) {
+        iterateIfArray(arr[i])
+      }
+    } else {
+      flattenedArray.push(arr)
+    }
+  }
+
+  iterateIfArray(array)
+  return flattenedArray
 }
 
 module.exports = {
@@ -104,4 +135,4 @@ module.exports = {
   flatten,
   stringReverse,
   range
-};
+}
